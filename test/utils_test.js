e92fbef223b25a0b50a59729d8191ef704902576
Mark Wolfe <mark@wolfe.id.au>
2015-02-05 13:50:32 +1100

    Added tests and some fixtures for different results.

---
+++ test/utils_test.js
@@ -0,0 +1,24 @@
+var when = require('when');
+var expect = require('chai').expect;
+
+var restUtils = require('../lib/web/rest/utils');
+var things = require('./fixtures/combine_things.json');
+var devices = require('./fixtures/combine_devices.json');
+var channels = require('./fixtures/combine_channels.json');
+
+
+describe('utils', function () {
+
+  it('should merge devices and channels into things', function(){
+
+    return when.all([
+      restUtils.combineThings.call(this, things, devices, channels)
+    ]).spread(function(result){
+      console.log('result', result);
+      expect(result[0].device).to.exist;
+      expect(result[0].device.channels).to.exist;
+    });
+
+  });
+
+});
